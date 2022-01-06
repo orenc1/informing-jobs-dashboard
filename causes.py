2b3d17f559260399a7f1f74e916850c5c665de79
orenc1 <ocohen@redhat.com>
2022-01-06 11:51:03 +0200

    add more classifications
    Signed-off-by: orenc1 <ocohen@redhat.com>

--- causes.py
+++ causes.py
@@ -1,10 +1,13 @@
 causes_list = {
-    "fatal msg=Bootstrap failed to complete": "Bootstrap failure (OCP)",
-    "failed to get CLI image: unable to wait for the 'cli' image in the stable stream to populate: timed out waiting for the condition": "Bootstrap failure (CI)",
-    "pod \"e2e-azure-deploy-cnv-ipi-install-install\" failed": "Bootstrap failure (OCP)",
-    "level=fatal msg=failed to fetch Cluster: failed to generate asset": "Bootstrap failure (Azure)",
-    "[Fail] [rfe_id:273]": "KubeVirt Tests failure",
-    "error: timed out waiting for the condition on pods/disks-images-provider": "disks-images-provider timeout",
-    "--- FAIL: TestTests ": "KubeVirt Tests failure",
-    "Process did not finish before 2h0m0s timeout": "CNV deploy failure"
+    "fatal msg=Bootstrap failed to complete": "[OCP] Bootstrap failed to complete",
+    "level=fatal msg=failed to initialize the cluster": "[OCP] failed to initialize the cluster",
+    "failed to get CLI image: unable to wait for the 'cli' image in the stable stream to populate: timed out waiting for the condition": "[OCP-CI] failed to get CLI image",
+    "pod \"e2e-azure-deploy-cnv-ipi-install-install\" failed": "[OCP] Bootstrap failure",
+    "level=error msg=\"failed to list bundles\"": "[OCP-OLM]: failed to list bundles",
+    "level=fatal msg=failed to fetch Cluster: failed to generate asset": "[Azure] Bootstrap failure - failed to fetch Cluster",
+    "[Fail] [rfe_id:273]": "[CNV] KubeVirt Tests failure",
+    "error: timed out waiting for the condition on pods/disks-images-provider": "[CNV] disks-images-provider timeout",
+    "--- FAIL: TestTests ": "[CNV] KubeVirt Tests failure",
+    "Process did not finish before 2h0m0s timeout": "[CNV] deployment failure",
+    "send: spawn id exp5 not open": "[CNV] VM Uptime test failure",
 }
